d3249b57fc1aa209d479ab7f61ff60c9b5261399
moura <moura3950@gmail.com>
2021-01-01 17:24:56 -0300

    update: Version 2.20

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ import setuptools
 
 setuptools.setup(name="music_sender", 
     packages=["music_sender", "music_sender.scripts", "music_sender.gui"], 
-    version="2.1.0", 
-    author="moura", 
+    version="2.2.0",
+    author="moura",
     author_email="moura3950@gmail.com",
-    description="A little app to send musics dinamically.",
-    )
+    description="A little app to send musics dinamically.",)
