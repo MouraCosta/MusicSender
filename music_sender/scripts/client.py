4953575409261a93d322f5d96b0f3ee413f42662
moura <moura3950@gmail.com>
2021-03-07 16:00:49 -0300

    Fix client error

--- music_sender/scripts/client.py
+++ music_sender/scripts/client.py
@@ -10,24 +10,36 @@ import os
 import random
 import socket
 import sys
-import time
+
 from . import utils
 
 
 def checkout(server_data):
-    """Make a little check to see if the server has sent 
-    not-available command."""
+    """Check for error messages from the server."""
     if server_data == "not-available":
         print("There's no music on the server")
         sys.exit(0)
+    elif server_data == "bad-parameter":
+        print("\033[;31merror: Invalid parameter\033[m")
+        sys.exit(1)
+
+
+def raw_available(client) -> list:
+    """Returns a list of musics."""
+    client.send(b"--raw-available")
+    server_musics = ""
+    while True:
+        music_name = client.recv(4096).decode()
+        if music_name == "end":
+            break
+        checkout(music_name)
+        server_musics += music_name
+    return server_musics.split("|")
 
 
 def available(client):
-    """Get the available music catalog in the server."""
-    client.send(b"--raw-available")
-    server_musics = client.recv(4096).decode("utf8")
-    checkout(server_musics)
-    server_musics = server_musics.split("|")
+    """Print out the available musics on the server."""
+    server_musics = raw_available(client)
     for i, msc in enumerate(server_musics):
         print(f"{i + 1} -> {msc}")
 
@@ -39,7 +51,9 @@ def copy(client, option):
     checkout(music_name)
     print(f"Creating {music_name}")
     with open(music_name, "wb") as music_file:
-        music_file.write(client.makefile("rb").read())
+        client_file = client.makefile("rb")
+        music_file.write(client_file.read())
+        client_file.close()
     print(f"{music_name} created with success.")
 
 
@@ -47,9 +61,7 @@ def diff(client):
     """Yields the relative index and the file name of the missing music
     files in client."""
     client.send(b"--raw-available")
-    server_mscs = client.recv(4096).decode("utf8")
-    checkout(server_mscs)
-    server_mscs = server_mscs.split("|")
+    server_mscs = raw_available(client)
     client_mscs = [file for file in os.listdir(
         ".") if utils.is_music_file(file)]
     missing_client_mscs = list(
@@ -59,39 +71,42 @@ def diff(client):
         yield relative_index, missing
 
 
-def automatic(client):
-    """Make an update in your music catalog."""
+def automatic(client, address):
+    """Make an automatic action. It download all the remaining musics."""
     for info in diff(client):
-        copy(client, info[0])
-        time.sleep(0.2)
+        copy_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        copy_client.connect(address)
+        copy(copy_client, info[0])
 
 
 def handle_args(client, args):
     """This function is responsible by handling the arguments."""
-    if args.available and not args.diff:
+    if args.available and not (args.diff or args.copy or args.automatic):
         available(client)
-    elif args.copy:
+    elif args.copy and not (args.diff or args.automatic or args.available):
         option = args.copy
         copy(client, option)
-    elif args.automatic and not args.diff:
-        automatic(client)
-    elif args.diff:
+    elif args.automatic and not (args.available or args.copy or args.diff):
+        automatic(client, (args.host, args.port))
+    elif args.diff and not (args.available or args.copy or args.automatic):
         for i, mssng in diff(client):
             print(f"{i} -> {mssng}")
+    else:
+        # Happens if the user try to mix options
+        print("\033[;31mDon't mix options, only put the necessary\033[m")
 
 
-def set_ambient(client, path, host, port) -> bool:
-    """Try to connect to the server, it automatically exits when no
-    server was available. Returns True when the ambient was succesfully
-    set, otherwise returns False."""
+def set_ambient(client, path, address) -> bool:
+    """Try to connect to the server. Returns True when the ambient was 
+    succesfully set, otherwise returns False."""
     try:
-        client.connect((host, port))
+        client.connect((address[0], address[1]))
         os.chdir(path)
     except (ConnectionRefusedError, NotADirectoryError, PermissionError,
             FileNotFoundError) as err:
-        if err in (ConnectionRefusedError, NotADirectoryError,
+        if err in (FileNotFoundError, NotADirectoryError,
                    PermissionError):
-            # When the error came from the change_directory function
+            # When the error came from the chdir() or .connect() functions.
             print("An error has ocurred")
             print("Possible issues:\n\t-You're not root\n\t-Directory "
                   "not found or its not a directory")
@@ -104,29 +119,29 @@ def set_ambient(client, path, host, port) -> bool:
 
 
 def add_arguments(parser):
-    """Add the arguments for the client applet."""
+    """Add the arguments for the client app."""
     parser.add_argument("-v", "--available", help="shows the available "
-                        "music catalog and theirs code numbers "
-                        "(e.g 120, 0, 31)", action="store_true")
+                        "music catalog and theirs code numbers",
+                        action="store_true")
     parser.add_argument("-c", "--copy", help="download a music from a given "
-                        "option, should make nothing if the option is "
-                        "invalid", type=int)
+                        "option. In case of a wrong index, a simple error "
+                        "message is returned from the server", type=int)
     parser.add_argument("-d", "--diff", help="This shows a list of musics "
-                        "that in server but not in client music directory "
-                        "(sorry, I like math).\n This option is very useful "
-                        "when you want make a single copy of a music you "
-                        "don't have.",
+                        "that in server but not in client music directory. "
+                        "It uses the path specified by --local.",
                         action="store_true")
     parser.add_argument("-a", "--automatic", help="downloads the list of "
                         "musics that is not in client directory.",
                         action="store_true")
-    parser.add_argument("-l", "--local", help="This is the path the musics "
-                        "will be download, default is ./", default=".",
-                        type=str)
-    parser.add_argument("-hs", "--host", help="Server host", type=str,
+    parser.add_argument("-l", "--local", help="Path where musics will be "
+                        "stored. if not specified the local is the current "
+                        "path.", default=".", type=str)
+    parser.add_argument("-hs", "--host", help="Server host. if not specified, "
+                        "the host is localhost", type=str,
                         default=socket.gethostname())
-    parser.add_argument("-p", "--port", help="Server port", type=int,
-                        default=random.randrange(1, 65432))
+    parser.add_argument("-p", "--port", help="Server port. if not specified the"
+                        " port is a range between 1024 and 65432", type=int,
+                        default=random.randrange(1024, 65432))
 
 
 def main():
@@ -134,7 +149,8 @@ def main():
     add_arguments(parser)
     args = parser.parse_args()
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    was_succesful = set_ambient(client, args.local, args.host, args.port)
+    address = (args.host, args.port)
+    was_succesful = set_ambient(client, args.local, address)
     if was_succesful:
         handle_args(client, args)
     else:
